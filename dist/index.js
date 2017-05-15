f44d9c5be2e02952c5d94e428095821b13d5e11f
afeiship <1290657123@qq.com>
2017-05-15 17:12:24 +0800

    udpate export

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,22 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.plupload = undefined;
+exports.mOxie = exports.plupload = undefined;
 
 var _qiniuJs = require('qiniu-js');
 
 var _qiniuJs2 = _interopRequireDefault(_qiniuJs);
 
+var _plupload = require('plupload');
+
+var _plupload2 = _interopRequireDefault(_plupload);
+
+var _moxie = require('pluploadjs/moxie');
+
+var _moxie2 = _interopRequireDefault(_moxie);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = _qiniuJs2.default;
-var plupload = exports.plupload = require('plupload').default;
+exports.plupload = _plupload2.default;
+exports.mOxie = _moxie2.default;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import Qiniu from 'qiniu-js';
+import plupload from 'plupload';
+import mOxie from 'pluploadjs/moxie';
+
 
 export default Qiniu;
-export const plupload = require('plupload').default;
+export {
+  plupload,
+  mOxie
+}
