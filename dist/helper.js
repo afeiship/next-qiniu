586486dc29ee27da5521e200e62f2688c3c5a7d7
afeiship <1290657123@qq.com>
2017-05-15 18:32:14 +0800

    const to file

--- dist/helper.js
+++ dist/helper.js
@@ -27,32 +27,11 @@ var _q = require('q');
 
 var _q2 = _interopRequireDefault(_q);
 
+var _const = require('./const');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var defaultOptions = {
-  runtimes: 'html5',
-  uptoken_url: '/', // Ajax请求uptoken的Url，强烈建议设置（服务端提供）
-  get_new_uptoken: false, // 设置上传文件的时候是否每次都重新获取新的uptoken
-  domain: 'http://qiniu-plupload.qiniudn.com/',
-  container: 'qiniu_container', // 上传区域DOM ID，默认是browser_button的父元素
-  max_file_size: '100mb', // 最大文件体积限制
-  max_retries: 3, // 上传失败最大重试次数
-  dragdrop: false, // 开启可拖曳上传
-  unique_names: true,
-  chunk_size: '4mb', // 分块上传时，每块的体积
-  auto_start: true, // 选择文件后自动上传，若关闭需要自己绑定事件触发上传
-  multi_selection: false,
-  filters: {
-    mime_types: [{
-      title: "Image files",
-      extensions: "jpeg,jpg,gif,png"
-    }],
-    max_img_resolution: [1280, 960],
-    prevent_duplicates: false
-  }
-};
 
 var _default = function () {
   function _default() {
@@ -71,7 +50,7 @@ var _default = function () {
     key: 'uploader',
     value: function uploader(inOptions) {
       var deferred = _q2.default.defer();
-      _qiniuJs2.default.uploader((0, _objectAssign2.default)(defaultOptions, {
+      _qiniuJs2.default.uploader((0, _objectAssign2.default)({
         init: {
           FileUploaded: function FileUploaded(up, file, info) {
             deferred.resove(up, file, info);
@@ -80,7 +59,7 @@ var _default = function () {
             deferred.reject(up, err, errTip);
           }
         }
-      }, inOptions));
+      }, _const.DEFAULTS, inOptions));
       return deferred.promise;
     }
   }]);
--- src/helper.js
+++ src/helper.js
@@ -3,32 +3,7 @@ import objectAssign from 'object-assign';
 import plupload from 'plupload';
 import mOxie from 'plupload/js/moxie';
 import Q from 'q';
-
-const defaultOptions = {
-  runtimes: 'html5',
-  uptoken_url: '/',         // Ajax请求uptoken的Url，强烈建议设置（服务端提供）
-  get_new_uptoken: false,             // 设置上传文件的时候是否每次都重新获取新的uptoken
-  domain: 'http://qiniu-plupload.qiniudn.com/',
-  container: 'qiniu_container',             // 上传区域DOM ID，默认是browser_button的父元素
-  max_file_size: '100mb',             // 最大文件体积限制
-  max_retries: 3,                     // 上传失败最大重试次数
-  dragdrop: false,                     // 开启可拖曳上传
-  unique_names: true,
-  chunk_size: '4mb',                  // 分块上传时，每块的体积
-  auto_start: true,                   // 选择文件后自动上传，若关闭需要自己绑定事件触发上传
-  multi_selection: false,
-  filters: {
-    mime_types: [
-      {
-        title: "Image files",
-        extensions: "jpeg,jpg,gif,png"
-      }
-    ],
-    max_img_resolution: [1280, 960],
-    prevent_duplicates: false
-  }
-};
-
+import {DEFAULTS} from './const';
 
 export default class {
 
@@ -42,7 +17,7 @@ export default class {
   static uploader(inOptions) {
     const deferred = Q.defer();
     Qiniu.uploader(
-      objectAssign(defaultOptions, {
+      objectAssign({
         init: {
           FileUploaded: function (up, file, info) {
             deferred.resove(up, file, info);
@@ -51,7 +26,7 @@ export default class {
             deferred.reject(up, err, errTip);
           }
         }
-      }, inOptions)
+      }, DEFAULTS, inOptions)
     );
     return deferred.promise;
   }
